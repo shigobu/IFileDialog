0a0dcd98b3a4fff9dad72c6817a1247e0ef8174b
kasamatsu <kasamatsu@kasa.com>
2019-12-16 15:59:04 +0900

    整理

---
+++ IFileDialog/Native/IFileOpenDialog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMInterfaceWrapper.Native
+{
+    [ComImport]
+    [Guid("DC1C5A9C-E88A-4dde-A5A1-60F82A20AEF7")]
+    internal class FileOpenDialog
+    {
+    }
+
+    // not fully defined と記載された宣言は、支障ない範囲で端折ってあります。
+    [ComImport]
+    [Guid("42f85136-db7e-439c-85f1-e4075d135fc8")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IFileOpenDialog
+    {
+        [PreserveSig]
+        UInt32 Show([In] IntPtr hwndParent);
+        void SetFileTypes();     // not fully defined
+        void SetFileTypeIndex();     // not fully defined
+        void GetFileTypeIndex();     // not fully defined
+        void Advise(); // not fully defined
+        void Unadvise();
+        void SetOptions([In] FILEOPENDIALOGOPTIONS fos);
+        FILEOPENDIALOGOPTIONS GetOptions(); // not fully defined
+        void SetDefaultFolder(); // not fully defined
+        void SetFolder(IShellItem psi);
+        void GetFolder(); // not fully defined
+        void GetCurrentSelection(); // not fully defined
+        void SetFileName();  // not fully defined
+        void GetFileName();  // not fully defined
+        void SetTitle([In, MarshalAs(UnmanagedType.LPWStr)] string pszTitle);
+        void SetOkButtonLabel(); // not fully defined
+        void SetFileNameLabel(); // not fully defined
+        void GetResult(out IShellItem ppsi);
+        void AddPlace(); // not fully defined
+        void SetDefaultExtension(); // not fully defined
+        void Close(); // not fully defined
+        void SetClientGuid();  // not fully defined
+        void ClearClientData();
+        void SetFilter(); // not fully defined
+        void GetResults(); // not fully defined
+        void GetSelectedItems(); // not fully defined
+    }
+
+    [Flags]
+    public enum FILEOPENDIALOGOPTIONS : uint
+    {
+        FOS_OVERWRITEPROMPT = 0x2,
+        FOS_STRICTFILETYPES = 0x4,
+        FOS_NOCHANGEDIR = 0x8,
+        FOS_PICKFOLDERS = 0x20,
+        FOS_FORCEFILESYSTEM = 0x40,
+        FOS_ALLNONSTORAGEITEMS = 0x80,
+        FOS_NOVALIDATE = 0x100,
+        FOS_ALLOWMULTISELECT = 0x200,
+        FOS_PATHMUSTEXIST = 0x800,
+        FOS_FILEMUSTEXIST = 0x1000,
+        FOS_CREATEPROMPT = 0x2000,
+        FOS_SHAREAWARE = 0x4000,
+        FOS_NOREADONLYRETURN = 0x8000,
+        FOS_NOTESTFILECREATE = 0x10000,
+        FOS_HIDEMRUPLACES = 0x20000,
+        FOS_HIDEPINNEDPLACES = 0x40000,
+        FOS_NODEREFERENCELINKS = 0x100000,
+        FOS_OKBUTTONNEEDSINTERACTION = 0x200000,
+        FOS_DONTADDTORECENT = 0x2000000,
+        FOS_FORCESHOWHIDDEN = 0x10000000,
+        FOS_DEFAULTNOMINIMODE = 0x20000000,
+        FOS_FORCEPREVIEWPANEON = 0x40000000,
+        FOS_SUPPORTSTREAMABLEITEMS = 0x80000000
+    };
+}
